804d7209936110237e74b62d11009f681606015f
emolokova <yaroslav4ik@yandex.ru>
2021-03-10 15:34:20 +0100

    feature/classCard

--- scripts/index.js
+++ scripts/index.js
@@ -1,54 +1,10 @@
-const popups = document.querySelectorAll(".popup");
-const profileInfoForm = document.querySelector(".popup_profile-info-form");
-const photoAddingForm = document.querySelector(".popup_photo-adding-form");
-const viewingPhotoForm = document.querySelector(".popup_viewing-photo");
-const editBtn = document.querySelector(".profile__editbtn");
-const addBtn = document.querySelector(".profile__addbtn");
-const nameInput = document.querySelector(".popup__name");
-const jobInput = document.querySelector(".popup__job");
-const profileName = document.querySelector(".profile__name");
-const profileJob = document.querySelector(".profile__job");
-const placeNameInput = document.querySelector(".popup__placename");
-const linkInput = document.querySelector(".popup__link");
-const elementsContainer = document.querySelector(".elements");
-const templateElement = document.querySelector(".template");
-const widePhoto = document.querySelector(".popup__wide-photo");
-const widePhotoFigcaption = document.querySelector(".popup__figcaption");
-const closedBtnProfile = profileInfoForm.querySelector(".popup__closedbtn");
-const closedBtnPhoto = photoAddingForm.querySelector(".popup__closedbtn");
-const closedBtnWidePhoto = viewingPhotoForm.querySelector(".popup__closedbtn");
-const initialCards = [
-    {
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
-        alt: 'Архыз'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
-        alt: 'Челябинская область'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
-        alt: 'Иваново'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
-        alt: 'Камчатка'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
-        alt: 'Холмогорский район'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
-        alt: 'Байкал'
-    }
-];
+import {popups, profileInfoForm, photoAddingForm, viewingPhotoForm,
+    profileName, profileJob, editBtn, addBtn, nameInput, jobInput, placeNameInput, linkInput,
+    elementsContainer, templateElement, widePhoto, widePhotoFigcaption,
+    closedBtnProfile, closedBtnPhoto, closedBtnWidePhoto, initialCards
+} from "./data.js"
+
+/*
 
 function render() {
     const cards = initialCards
@@ -85,6 +41,8 @@ function getItem(item) {
 }
 
 render();
+
+*/
 
 function openPopup(popup) {
     popup.classList.add("popup_opened");
